13bc8eab68dfefe7fbcea44f9b04ec3a5e8c493b
ms <2495974489@qq.com>
2022-07-24 10:51:39 +0800

    first commit

---
+++ src/main/java/com/ms/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.ms.git;
+
+/**
+ * @author MS
+ * @create 2022-07-24-10:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
